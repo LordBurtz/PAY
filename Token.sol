47d294ca142a428d3dfec361937b52ff31ef8aca
LordBurtz <71921875+LordBurtz@users.noreply.github.com>
2021-11-19 22:26:12 +0000

    Create Token.sol

---
+++ Token.sol
@@ -0,0 +1,65 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.7.0;
+
+contract SafeMath {
+  function safeMul(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a * b;
+    assert(a == 0 || c / a == b);
+    return c;
+  }
+
+  function safeDiv(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b > 0);
+    uint256 c = a / b;
+    assert(a == b * c + a % b);
+    return c;
+  }
+
+  function safeSub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  function safeAdd(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c>=a && c>=b);
+    return c;
+  }
+}
+
+
+contract Token is SafeMath{
+    string public name;
+    string public symbol;
+    uint256 public decimals;
+    uint256 public totalSupply;
+	address public owner;
+
+    mapping (address => uint256) public balanceOf;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    constructor (
+        uint256 initialSupply,
+        string memory tokenName,
+        uint256 decimalUnits,
+        string memory tokenSymbol
+        ) {
+        balanceOf[msg.sender] = initialSupply;              // Give the creator all initial tokens
+        totalSupply = initialSupply;                        // Update total supply
+        name = tokenName;                                   // Set the name for display purposes
+        symbol = tokenSymbol;                               // Set the symbol for display purposes
+        decimals = decimalUnits;                            // Amount of decimals for display purposes
+		owner = msg.sender;
+    }
+
+    function transfer(address _to, uint256 _value) public {                          // Prevent transfer to 0x0 address. Use burn() instead
+		require(_value >= 0); 
+        require(balanceOf[msg.sender] >= _value);           // Check if the sender has enough
+        require(balanceOf[_to] + _value > balanceOf[_to]); // Check for overflows
+        balanceOf[msg.sender] = SafeMath.safeSub(balanceOf[msg.sender], _value);                     // Subtract from the sender
+        balanceOf[_to] = SafeMath.safeAdd(balanceOf[_to], _value);                            // Add the same to the recipient
+        emit Transfer(msg.sender, _to, _value);                   // Notify anyone listening that this transfer took place
+    }
+}
