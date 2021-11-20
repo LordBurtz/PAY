4b21775cc23f7eee784e7e3e00141cf2d9b051fd
LordBurtz <fridolin.karger@gmail.com>
2021-11-20 08:26:32 +0100

    constructor added; started deposit

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -7,12 +7,34 @@ import "./SafeMath.sol";
 
 contract Bank is IBank {
 
-    constructor(address _priceOracle, address _hakToken) {}
+    string public name;
+    string public symbol;
+    uint256 public decimals;
+	address public owner;
+	Loan public loan;
+	Loan[] public loans;
+    PriceOracleTest public oracle;
+
+mapping (address => uint256) public balanceOf;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    constructor(address _priceOracle, address _hakToken) {
+        balanceOf[msg.sender] = initialSupply;              // Give the creator all initial tokens                      // Update total supply
+        name = tokenName;                                   // Set the name for display purposes
+        symbol = tokenSymbol;                               // Set the symbol for display purposes
+        decimals = decimalUnits;                            // Amount of decimals for display purposes
+		owner = msg.sender;
+    }
     function deposit(address token, uint256 amount)
         payable
         external
         override
-        returns (bool) {}
+        returns (bool) {
+            require(msg.value == safeMul(oracle.getVirtualPrice(token), amount)); //afaik i understand it, it returns it scaled
+        uint256 scaledAmount = safeMul(oracle.getVirtualPrice(token), amount);
+        require(this.transfer(msg.sender, scaledAmount));
+        }
 
     function withdraw(address token, uint256 amount)
         external
@@ -28,7 +50,11 @@ contract Bank is IBank {
         payable
         external
         override
-        returns (uint256) {}
+        returns (uint256) {
+            require(msg.value == safeMul(oracle.getVirtualPrice(token), amount)); //afaik i understand it, it returns it scaled
+        uint256 scaledAmount = safeMul(oracle.getVirtualPrice(token), amount);
+        require(this.transfer(msg.sender, scaledAmount));
+        }
 
     function liquidate(address token, address account)
         payable
