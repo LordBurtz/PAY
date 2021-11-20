0f6415ade122fe58d9b285ade4f77f9845aac193
LordBurtz <fridolin.karger@gmail.com>
2021-11-20 09:02:17 +0100

    setup done

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -6,52 +6,36 @@ import "./interfaces/IPriceOracle.sol";
 import "./SafeMath.sol";
 
 contract Bank is IBank {
+string public name;
+    IPriceOracle public oracle;
 
-    string public name;
-    string public symbol;
-    uint256 public decimals;
-	address public owner;
-	Loan public loan;
-	Loan[] public loans;
-    PriceOracleTest public oracle;
-
-mapping (address => uint256) public balanceOf;
+    mapping (address => uint256) public balanceOf;
 
     event Transfer(address indexed from, address indexed to, uint256 value);
 
-    constructor(address _priceOracle, address _hakToken) {
-        balanceOf[msg.sender] = initialSupply;              // Give the creator all initial tokens                      // Update total supply
-        name = tokenName;                                   // Set the name for display purposes
-        symbol = tokenSymbol;                               // Set the symbol for display purposes
-        decimals = decimalUnits;                            // Amount of decimals for display purposes
-		owner = msg.sender;
+    constructor (address hack_coin, address _priceOracle) {
+        oracle = _priceOracle;
+
     }
-    function deposit(address token, uint256 amount)
-        payable
-        external
-        override
-        returns (bool) {
-            require(msg.value == safeMul(oracle.getVirtualPrice(token), amount)); //afaik i understand it, it returns it scaled
+
+
+    function deposit(address token, uint256 amount)payable external override returns (bool) {
+        
+        require(msg.value >= safeMul(oracle.getVirtualPrice(token), amount)); //afaik i understand it, it returns it scaled
         uint256 scaledAmount = safeMul(oracle.getVirtualPrice(token), amount);
-        require(this.transfer(msg.sender, scaledAmount));
-        }
+        return require(this.transfer(msg.sender, scaledAmount));
+    }
 
-    function withdraw(address token, uint256 amount)
-        external
-        override
-        returns (uint256) {}
+    function deposit(uint256 amount) payable external override returns(bool) {
+        
+    }
 
-    function borrow(address token, uint256 amount)
-        external
-        override
-        returns (uint256) {}
+    function withdraw(address token, uint256 amount) external override returns (uint256) {}
 
-    function repay(address token, uint256 amount)
-        payable
-        external
-        override
-        returns (uint256) {
-            require(msg.value == safeMul(oracle.getVirtualPrice(token), amount)); //afaik i understand it, it returns it scaled
+    function borrow(address token, uint256 amount) external override returns (uint256) {}
+
+    function repay(address token, uint256 amount) payable external override returns (uint256) {
+        require(msg.value == safeMul(oracle.getVirtualPrice(token), amount)); //afaik i understand it, it returns it scaled
         uint256 scaledAmount = safeMul(oracle.getVirtualPrice(token), amount);
         require(this.transfer(msg.sender, scaledAmount));
         }
